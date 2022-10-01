51eff384172c340db54a7d90ec9a7bbb0e40fdab
hafsa <hfsyln93@gmail.com>
2022-10-02 00:58:52 +0300

    edit alarm

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ function getTime(){
     let second = time.getSeconds();
         
 
-    let day = time.getDay();
+    let day = time.getDate();
+    console.log(time)
+    console.log(day)
     let month = time.getMonth();
     let year = time.getFullYear();
+    let günler = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "C.tesi"]
+    let haftagün = (günler[time.getDay()])
     if(second <= 9){
         second = "0" + second
     };
@@ -19,6 +23,16 @@ function getTime(){
     document.querySelector(".minutes").textContent = minute
     document.querySelector(".second").textContent = second
     
-    document.querySelector(".date").textContent = day + "." + (+month + 1)+ "." + year
+    document.querySelector(".date").textContent = day + "." + (+month + 1)+ "." + year + " " + haftagün
 }
 setInterval(function(){getTime();},1000); //kodumuzu hangi sıklıkla çalıştıracağımızı yazar setInterval
+
+
+if(hour == document.querySelector(".saat").value && minute == document.querySelector(".dakika").value && haftagün ==document.querySelector(".gün").value ){
+
+    foo();
+    function foo() {
+      body.style.background = "red";
+
+      audio.src = "ses indir ve ekle";
+}
