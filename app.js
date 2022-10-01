2ba77f0103f2428392f5a6cd765305ed4fb24ed8
hafsa <hfsyln93@gmail.com>
2022-10-01 23:09:52 +0300

    edit

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ function getTime(){
     let hour = time.getHours();
     let minute = time.getMinutes();
     let second = time.getSeconds();
+        
 
     let day = time.getDay();
     let month = time.getMonth();
     let year = time.getFullYear();
+    if(second <= 9){
+        second = "0" + second
+    };
+    if(minute <= 9){
+        minute = "0" + minute
+    };
 
     document.querySelector(".hour").textContent = hour
     document.querySelector(".minutes").textContent = minute
     document.querySelector(".second").textContent = second
-
+    
     document.querySelector(".date").textContent = day + "." + (+month + 1)+ "." + year
 }
 setInterval(function(){getTime();},1000); //kodumuzu hangi sıklıkla çalıştıracağımızı yazar setInterval
