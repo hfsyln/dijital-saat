55bca21420867b1454ad4b1f38ff7d4b604e3d59
hafsa <hfsyln93@gmail.com>
2022-10-03 17:24:25 +0300

    edit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let ses = document.querySelector("audio");
 let buton = document.querySelector(".buton");
 let buton2 = document.querySelector(".buton2");
+let butonactv = false;
+
+
 function getTime(){
     let time = new Date();
     let hour = time.getHours();
@@ -25,37 +28,62 @@ function getTime(){
     document.querySelector(".date").textContent = day + "." + (+month + 1)+ "." + year + " " + haftagün
 
     let zaman = `${hour}:${minute}`
-    console.log(zaman)
+ 
 
     let alarm = `${document.querySelector(".saat").value}:${document.querySelector(".dakika").value}`
-    console.log(alarm)
+    
 
 
 console.log(buton)
+
 buton.addEventListener("click", ()=> {
     console.log(   "buton cilick")
-    if(document.querySelector(".saat").value && document.querySelector(".dakika").value ){
-        document.querySelector(".uyarı").innerHTML = `Alarm Kurulmuştur`
-        setTimeout(() => {
-            document.querySelector(".uyarı").innerHTML = "";
-          }, "5000")
-        document.querySelector(".saat").readOnly = true;
-        document.querySelector(".dakika").readOnly = true;
-        if(zaman == alarm){
-            ses.play();
-            console.log(ses.scr);
-        }
-    }
+    console.log(buton.textContent)
+    
 
-    if(document.querySelector(".saat").value == "" || document.querySelector(".dakika").value == ""){
-        document.querySelector(".uyarı").innerHTML = `<div >Geçerli bir zaman giriniz</div>`
-    }
-    
-    
+   
+        if(document.querySelector(".saat").value && document.querySelector(".dakika").value && !butonactv) {
+            document.querySelector(".uyarı").innerHTML = `Alarm Kurulmuştur`
+            buton.textContent = "Stop"
+            buton.style.backgroundColor = "green"
+            
+            setTimeout(() => {
+                document.querySelector(".uyarı").innerHTML = "";
+            }, "5000")
+            document.querySelector(".saat").readOnly = true;
+            document.querySelector(".dakika").readOnly = true;
+            if(zaman == alarm){
+                ses.play();
+                console.log(ses.scr);
+            }
+            butonactv = true
+            console.log(butonactv)
+        }else if(document.querySelector(".saat").value == "" || document.querySelector(".dakika").value == ""){
+            document.querySelector(".uyarı").innerHTML = `<div >Geçerli bir zaman giriniz</div>`
+            console.log(butonactv)
+        }else if(butonactv){
+            document.querySelector(".uyarı").innerHTML = `<div >Alarm İptal Edilmiştir</div>`
+            setTimeout(() => {
+                document.querySelector(".uyarı").innerHTML = "";
+            }, "5000")
+            document.querySelector(".saat").readOnly = false;
+            document.querySelector(".dakika").readOnly = false;
+            buton.textContent = "Active"
+            buton.style.backgroundColor = "coral"
+            butonactv = false
+            console.log(butonactv)
+        }
+        console.log(butonactv)
+      
+
 })
+
+
 buton2.addEventListener("click", ()=>{
     document.querySelector(".saat").readOnly = false;
-        document.querySelector(".dakika").readOnly = false;
+    document.querySelector(".dakika").readOnly = false;
+    document.querySelector(".saat").value = " "
+    document.querySelector(".dakika").value = " "
 
 })
 }
